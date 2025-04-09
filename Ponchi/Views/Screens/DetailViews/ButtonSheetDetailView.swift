ba836800736b106d9ff4c69c23b06e12056639bf
mary romanova <athostheprince@icloud.com>
2025-04-09 21:34:53 +0300

    Удалены лишние файлы и добавлен .gitignore

---
+++ Ponchi/Views/Screens/DetailViews/ButtonSheetDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonSheetDetailView.swift
+//  Ponchi
+//
+//  Created by mary romanova on 10.05.2025.
+//
+
+import SwiftUI
+
+struct ButtonSheetDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonSheetDetailView()
+}
